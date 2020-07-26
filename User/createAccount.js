55de9c739ce67aacdc7c8ab542596080673baf5c
Muhammet Şen <enmuhammet23@gmail.com>
2020-07-26 18:11:20 +0300

    Sign up and database connections

---
+++ User/createAccount.js
@@ -0,0 +1,28 @@
+async function isValid(req) {
+  const mainApp = require("../index");
+  var collection = mainApp.database.collection("Users");
+  var User = {
+    name: req.body.name,
+    email: req.body.email,
+    dateOfBirth: req.body.dateOfBirth,
+    username: req.body.username,
+    password: req.body.password,
+  };
+  response = await collection
+    .find({ $or: [{ username: User.username }, { email: User.email }] })
+    .toArray();
+
+  if (response.length != 0) {
+    if (response[0].username == User.username) {
+      return "Username has already been taken";
+    } else {
+      return "Email has already taken.";
+    }
+  }
+  // console.log(`${User.username} is valid`);
+  collection.insertOne(User);
+  return "Success";
+}
+module.exports = {
+  createUser: isValid,
+};
--- index.js
+++ index.js
@@ -1,13 +1,42 @@
-const express = require('express');
+const { Db } = require("mongodb");
+var database;
+console.log("Connecting to DB");
+const MongoClient = require("mongodb").MongoClient;
+const MongoURL = "mongodb://35.209.140.129:27018/Twitter";
+const client = new MongoClient(MongoURL, {
+  useUnifiedTopology: true,
+  useNewUrlParser: true,
+});
+client.connect().then((database = client.db("Twitter")));
+
+const express = require("express");
 const app = express();
+var createAccount = require("./User/createAccount");
+const cors = require("cors");
+app.use(cors());
 
-app.get("/", (req,res) =>{
-    res.json({ 
-        message:"meower",
-    });
+app.use(express.json());
+app.get("/", (req, res) => {
+  res.json({
+    message: "meower",
+  });
+});
+
+app.post("/mews", (req, res) => {
+  res.json({ message: "Done" });
+});
+
+app.post("/createAccount", async (req, res) =>  {
+  var status = await createAccount.createUser(req);
+  res.json({ message: status });
 });
 
 
-app.listen(8080,() => {
-    console.log("Listening on 8080"); 
+
+app.listen(8080, () => {
+  console.log("Listening on 8080");
 });
+
+module.exports = {
+  database: database,
+};
