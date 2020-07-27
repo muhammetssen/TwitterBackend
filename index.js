5b03a83230968fc6cd49423b973f6814f37de133
Muhammet Şen <enmuhammet23@gmail.com>
2020-07-28 00:27:41 +0300

    Added new post routes

--- index.js
+++ index.js
@@ -8,11 +8,11 @@ const client = new MongoClient(MongoURL, {
   useNewUrlParser: true,
 });
 client.connect().then((database = client.db("Twitter")));
-
 const express = require("express");
 const app = express();
 var createAccount = require("./User/createAccount");
 var logIn = require("./User/LogIn");
+var tweets = require("./Tweets/Tweet");
 const cors = require("cors");
 app.use(cors());
 
@@ -27,17 +27,49 @@ app.post("/mews", (req, res) => {
   res.json({ message: "Done" });
 });
 
-app.post("/createAccount", async (req, res) =>  {
+app.post("/createAccount", async (req, res) => {
   var status = await createAccount.createUser(req);
   res.json({ message: status });
 });
 
-app.post('/logIn', async (req,res) => {
-    var status = await logIn.logIn(req);
-    res.json({ message : status});
-
+app.post("/logIn", async (req, res) => {
+  var status = await logIn.logIn(req);
+  res.json({ message: status });
 });
 
+app.post("/rttweet", async (req, res) => {
+  tweets.retweet("5f1f263b9c62444d844a109b", "5f1f079ee2320d1804ed3b87");
+  res.json({});
+});
+
+app.post("/liketweet", async (req, res) => {
+  tweets.like("5f1f263b9c62444d844a109b", "5f1f0791e2320d2704ed3b87");
+  res.json({});
+});
+
+app.post("/getRTCount", async (req, res) => {
+  var response = await tweets.getRetweetCount(req.body.tweetId);
+  res.json({ message: response });
+});
+
+app.post("/getLikedCount", async (req, res) => {
+  var response = await tweets.getLikedCount(req.body.tweetId);
+  res.json({ message: response });
+});
+app.post("/getTweet", async (req, res) => {
+  var response = await tweets.getTweet(req.body.tweetId);
+  res.json({ message: response });
+});
+
+app.post("/getAllTweets", async (req, res) => {
+  var response = await tweets.getAllTweets();
+  res.json({ message: response });
+});
+
+app.post("/createTweet", (req, res) => {
+  tweets.createTweet("First Tweet", "5f1f07a0e3320d2804ed3b88", "today");
+  res.json({});
+});
 
 app.listen(8080, () => {
   console.log("Listening on 8080");
